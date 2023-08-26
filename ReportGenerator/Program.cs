5ca2c30f5469ba916e72ef7cf3e56cf41b74cde6
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-08-26 23:57:11 +0500

    Реализация отчетов с помощью делегирования

---
+++ ReportGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿using ReportGenerator;
+
+List<Measurement> data = new List<Measurement>
+		{
+			new Measurement
+			{
+				Humidity=1,
+				Temperature=-10,
+			},
+			new  Measurement
+			{
+				Humidity=2,
+				Temperature=2,
+			},
+			new Measurement
+			{
+				Humidity=3,
+				Temperature=14
+			},
+			new Measurement
+			{
+				Humidity=2,
+				Temperature=30
+			}
+		};
+
+var actual = ReportMakerHelper.MeanAndStdHtmlReport(data);
+Console.WriteLine(actual);
