2db4037cc1dafd7ec2ebdd8f5354c61b5c16d9de
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-08-27 20:31:00 +0500

    Удаление лишних классов

--- PairsAnalysis/Analysis.cs
+++ PairsAnalysis/Analysis.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace PairsAnalysis;
 
-namespace PairsAnalysis
+public static class Analysis
 {
-	internal class Analysis
+	public static int FindMaxPeriodIndex(params DateTime[] data)
 	{
+		if (data.Count() == 0)
+			throw new InvalidOperationException();
+		return data
+			.Pairs()
+			.Select((pair) => (pair.Item2 - pair.Item1).TotalSeconds)
+			.MaxIndex();
+	}
+
+	public static double FindAverageRelativeDifference(params double[] data)
+	{
+		if (data.Count() <= 1)
+			throw new InvalidOperationException();
+		var relativeDifferences = data
+			.Pairs()
+			.Select((pair) => (pair.Item2 - pair.Item1) / pair.Item1);
+		return relativeDifferences.Sum() / relativeDifferences.Count();
 	}
 }
+
+public static class ExtentionsAnalysis
+{
+	public static IEnumerable<Tuple<T, T>> Pairs<T>(this IEnumerable<T> dates)
+	{
+		var flag = true;
+		var previous = default(T);
+		foreach (var item in dates)
+		{
+			if (flag)
+			{
+				previous = item;
+				flag = false;
+				continue;
+			}
+			yield return Tuple.Create(previous, item);
+			previous = item;
+		}
+	}
+
+	public static int MaxIndex<T>(this IEnumerable<T> data)
+	where T : IComparable<T>
+	{
+		return data.Select((x, y) => Tuple.Create(x, y))
+			.OrderByDescending(x => x.Item1)
+			.First()
+			.Item2;
+	}
+}
+
+
+
