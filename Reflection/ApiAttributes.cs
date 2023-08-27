0683ab53feed7067c065d725bf0161034115a0d1
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-08-28 03:04:47 +0500

    Написаны 4 метода Specifier<T>

--- Reflection/ApiAttributes.cs
+++ Reflection/ApiAttributes.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Reflection;
 
-namespace Reflection
+[AttributeUsage(AttributeTargets.Method)]
+public class ApiMethodAttribute : Attribute
 {
-	internal class ApiAttributes
+}
+
+public class ApiDescriptionAttribute : Attribute
+{
+	public ApiDescriptionAttribute(string description)
 	{
+		Description = description;
 	}
+
+	public string Description { get; }
+}
+
+[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+public class ApiIntValidationAttribute : Attribute
+{
+	public ApiIntValidationAttribute(int minValue, int maxValue)
+	{
+		MaxValue = maxValue;
+		MinValue = minValue;
+	}
+
+	public int? MaxValue { get; }
+	public int? MinValue { get; }
+}
+
+[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+public class ApiRequiredAttribute : Attribute
+{
+	public ApiRequiredAttribute(bool required = true)
+	{
+		Required = required;
+	}
+
+	public bool Required { get; }
 }
--- Reflection/Specifier.cs
+++ Reflection/Specifier.cs
@@ -1,12 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Reflection;
 
-namespace Reflection
+namespace Reflection;
+
+public class Specifier<T> : ISpecifier
 {
-	internal class Specifier
+	public string? GetApiDescription() => typeof(T).GetCustomAttributes()
+				.OfType<ApiDescriptionAttribute>().FirstOrDefault()?.Description;
+
+	public string[] GetApiMethodNames()
 	{
+		return typeof(T).GetMethods().
+			Where(x => x.GetCustomAttributes().OfType<ApiMethodAttribute>().Any()).
+			Select(x => x.Name)
+			.ToArray();
+	}
+
+	public string GetApiMethodDescription(string methodName)
+	{
+		var method = typeof(T).GetMethods().Where(x => x.GetCustomAttributes().OfType<ApiMethodAttribute>().Any())
+			.Where(x => x.GetCustomAttributes().OfType<ApiDescriptionAttribute>().Any())
+			.Where(x => x.Name == methodName)
+			.FirstOrDefault();
+
+		if (method is null)
+			return null;
+		
+		return method.GetCustomAttributes().OfType<ApiDescriptionAttribute>().First().Description;
+	}
+
+	public string[] GetApiMethodParamNames(string methodName)
+	{
+		return typeof(T).GetMethods().Where(x => x.GetCustomAttributes().OfType<ApiMethodAttribute>().Any())
+			.Where(x => x.Name == methodName)
+			.FirstOrDefault()
+			.GetParameters()
+			.Select(x => x.Name.ToString())
+			.ToArray();
+
+	}
+
+	public string GetApiMethodParamDescription(string methodName, string paramName)
+	{
+		var parametr = typeof(T).GetMethods().Where(x => x.GetCustomAttributes().OfType<ApiMethodAttribute>().Any())
+			.Where(x => x.Name == methodName)
+			.FirstOrDefault()
+			.GetParameters()
+			.Where(x => x.Name == paramName)
+			.FirstOrDefault();
+			
+		return parametr.GetCustomAttributes().OfType<ApiDescriptionAttribute>().First().Description;
+	}
+
+	public ApiParamDescription GetApiMethodParamFullDescription(string methodName, string paramName)
+	{
+		throw new NotImplementedException();
+	}
+
+	public ApiMethodDescription GetApiMethodFullDescription(string methodName)
+	{
+		throw new NotImplementedException();
 	}
 }
+
